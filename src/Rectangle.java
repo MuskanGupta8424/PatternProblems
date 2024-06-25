ff22350255b3d51a09080e5108e861043cb08241
MuskanGupta8424 <mg8303655@gmail.com>
2024-06-25 15:06:36 +0530

    rectangle Pattern

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        System.out.println("Rectangle Pattern ");
+        for(int i=1; i<=3; i++){
+
+            for(int j=1;j<=6; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+}
